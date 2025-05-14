df55bfc22c520764f3888d34cf26bd80dab134a2
Papinwit rattanasiri <papinwitrat@hotmail.com>
2025-05-14 16:35:32 +0700

    make it compt with torchscript

--- model.py
+++ model.py
@@ -10,23 +10,32 @@ import os
 
 class CausalConv1d(torch.nn.Conv1d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, groups=1, bias=True):
-        self.__padding = (kernel_size - 1) * dilation
+        self._padding = (kernel_size - 1) * dilation
 
         super(CausalConv1d, self).__init__(
             in_channels,
             out_channels,
             kernel_size=kernel_size,
             stride=stride,
-            padding=self.__padding,
+            padding=self._padding,
             dilation=dilation,
             groups=groups,
             bias=bias,
         )
 
     def forward(self, input):
-        result = super(CausalConv1d, self).forward(input)
-        if self.__padding != 0:
-            return result[:, :, : -self.__padding]
+        result = torch.nn.functional.conv1d(
+            input,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups
+        )
+
+        if self._padding != 0:
+            return result[:, :, : -self._padding]
         return result
 
 
